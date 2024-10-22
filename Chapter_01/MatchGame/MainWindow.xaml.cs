615cfc8298f3de523d58530fd5c42ee2034b61c8
Abdallah Bakr <abab231196@gmail.com>
2024-10-22 10:24:13 +0300

    Move project

---
+++ Chapter_01/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			SetUpGame();
+		}
+
+		private void SetUpGame()
+		{
+			var animalEmoji = new List<string>()
+			{
+				"🐱", "🐱",
+				"🦒", "🦒",
+				"🐰", "🐰",
+				"🦓", "🦓",
+				"🐶", "🐶",
+				"🐻", "🐻",
+				"🐴", "🐴",
+				"🐒", "🐒"
+			};
+
+			var random = new Random();
+			foreach (var textBlock in MainGrid.Children.OfType<TextBlock>())
+			{
+				var index = random.Next(animalEmoji.Count);
+				var nextEmoji = animalEmoji[index];
+				textBlock.Text = nextEmoji;
+				animalEmoji.RemoveAt(index);
+			}
+		}
+	}
+}
