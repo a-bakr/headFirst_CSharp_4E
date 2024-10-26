a9afaf6ff7b0944210b5fbe4491e669a41020146
Abdallah Bakr <abab231196@gmail.com>
2024-10-26 17:17:25 +0300

    add card game to the repo

---
+++ Chapter_03/PickRandomCards/Program.cs
@@ -0,0 +1,13 @@
+﻿using PickRandomCards;
+
+Console.Write("Enter the number of cards to pick: ");
+var input = Console.ReadLine()?.TrimStart().TrimEnd();
+
+if (int.TryParse(input, out int numberOfCard))
+{
+	var pickedCards = CardPicker.PickSomeCard(numberOfCard);
+	foreach (var pickedCard in pickedCards)
+	{
+		Console.WriteLine(pickedCard);
+	}
+}
