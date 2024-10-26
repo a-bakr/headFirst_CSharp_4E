e0dceda630edf3e7333a3fc6d6c402d2af5e3ee9
Abdallah Bakr <abab231196@gmail.com>
2024-10-26 16:41:44 +0300

    finish experiment with control

--- Chapter_02/ExperimentWithControls/MainWindow.xaml.cs
+++ Chapter_02/ExperimentWithControls/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ExperimentWithControls
 {
@@ -19,6 +12,51 @@ namespace ExperimentWithControls
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			Number.Text = NumberTextBox.Text;
+		}
+
+		private void NumberTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+		{
+			e.Handled = !int.TryParse(e.Text, out int result);
+		}
+
+		private void RadioButton_Checked(object sender, RoutedEventArgs e)
+		{
+			if (sender is RadioButton radioButton)
+				Number.Text = radioButton.Content.ToString();
+		}
+
+		private void SmallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			Number.Text = SmallSlider.Value.ToString("0");
+		}
+
+		private void BigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			Number.Text = BigSlider.Value.ToString("000-000-0000");
+		}
+
+		private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (sender is ListBox listBox)
+			{
+				var item = listBox.SelectedItem as ListBoxItem;
+				Number.Text = item?.Content.ToString();
+			}
+		}
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (sender is ComboBox comboBox)
+			{
+				var item = comboBox.SelectedItem as ListBoxItem;
+				Number.Text = item?.Content.ToString();
+			}
+
 		}
 	}
 }
