27cbf1ee3414257be47561b8bc272dd039d8ff06
Abdallah Bakr <abab231196@gmail.com>
2024-10-28 07:29:26 +0300

    add Casino

---
+++ Chapter_03/Casino/Player.cs
@@ -0,0 +1,68 @@
+﻿namespace Casino;
+
+public class Player
+{
+	public string Name { get; set; }
+	public int Cash { get; set; }
+	public Player(string name, int cash)
+	{
+		Name = name;
+		Cash = cash;
+	}
+
+	/// <summary>
+	/// Writes my name and the amount of cash I have to the console
+	/// </summary>
+	public void WriteMyInfo()
+	{
+		var text = $"{Name} has {Cash} bucks";
+		Console.WriteLine(text);
+	}
+
+	/// <summary> 
+	/// Gives some of my cash, removing it from my wallet (or printing
+	/// a message to the console if I don't have enough cash).
+	/// </summary>
+	/// <param name="amount">Amount of cash to give.</param>
+	/// <returns>
+	///	The amount of cash removed from my wallet, or 0 if I don't
+	/// have enough cash (or if the amount is invalid).
+	/// </returns> 
+	public int GiveCash(int amount)
+	{
+		if (amount <= 0)
+		{
+			var text = $"{Name} says: {amount} isn't a valid amount";
+			Console.WriteLine(text);
+			return 0;
+		}
+
+		if (amount > Cash)
+		{
+			var text = $"{Name} says: I don't have enough cash to give you {amount}";
+			Console.WriteLine(text);
+			return 0;
+		}
+
+		Cash -= amount;
+		return amount;
+	}
+
+	/// <summary>
+	/// Receive some cash adding it to my wallet (or printing
+	/// a message to the console if the amount is invalid).
+	/// </summary>
+	/// <param name="amount">Amount of cash to get.</param>
+	public void ReceiveCash(int amount)
+	{
+		if (amount <= 0)
+		{
+			var text = $"{Name} says: {amount} isn't an amount I'll take";
+			Console.WriteLine(text);
+		}
+		else
+		{
+			Cash += amount;
+		}
+	}
+}
---
+++ Chapter_03/Casino/Program.cs
@@ -0,0 +1,38 @@
+﻿using Casino;
+
+var random = new Random();
+var odds = 0.75;
+var player = new Player("The player", 100);
+
+
+Console.WriteLine($"Welcome to the casino. The odds are {odds}");
+
+while (player.Cash > 0)
+{
+	player.WriteMyInfo();
+	Console.Write("How much do you want to bet: ");
+	var input = Console.ReadLine();
+	if (input == "") break;
+
+	if (int.TryParse(input, out int amount))
+	{
+		var pot = player.GiveCash(amount) * 2;
+		if (pot > 0)
+		{
+			if (random.NextDouble() > odds)
+			{
+				Console.WriteLine($"You Win {pot}");
+				player.ReceiveCash(pot);
+			}
+			else
+			{
+				Console.WriteLine($"Bad luck, you lose.");
+			}
+		}
+	}
+	else
+	{
+		Console.WriteLine("Please enter a valid number.");
+	}
+}
+Console.WriteLine("The house always wins");
