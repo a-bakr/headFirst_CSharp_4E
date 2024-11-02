6d558884bb8eb5898e9871c0f78bd8c534420c66
Abdallah Bakr <132272228+AbdallahBakr@users.noreply.github.com>
2024-11-02 13:53:25 +0200

    Add Elephant App

---
+++ Chapter_04/Elephant/Elephant.cs
@@ -0,0 +1,14 @@
+﻿namespace ElephantApp
+{
+    internal class Elephant(string name, int earSize)
+    {
+        public string Name = name;
+        public int EarSize = earSize;
+
+        public void WhoAmI()
+        {
+            Console.WriteLine($"My name is {Name}.");
+            Console.WriteLine($"My Ears are {EarSize} inches tall.");
+        }
+    }
+}
---
+++ Chapter_04/Elephant/Program.cs
@@ -0,0 +1,28 @@
+﻿using ElephantApp;
+
+var lucinda = new Elephant("Lucinda", 33);
+var lloyd = new Elephant("Lloyd", 40);
+
+Console.Write("Press 1 for Lloyd, 2 for Lucinda, 3 to swap: ");
+while (true)
+{
+    var key = Console.ReadKey(true).KeyChar;
+    Console.WriteLine($"You pressed {key}");
+    switch (key)
+    {
+        case '1':
+            Console.WriteLine("Calling Lloyd.WhoAMI()");
+            lloyd.WhoAmI();
+            break;
+        case '2':
+            Console.WriteLine("Calling Lucinda.WhoAMI()");
+            lucinda.WhoAmI();
+            break;
+        case '3':
+            (lucinda, lloyd) = (lloyd, lucinda);
+            Console.WriteLine("References have been swapped");
+            break;
+        default:
+            return;
+    }
+}
